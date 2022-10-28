45082a985e6e22542ab953e4ba8cdcd85727d69b
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-10-28 20:16:02 +0100

    update media querry

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import profileImage from "../../assets/images/profile.jpeg";
 import avatar from "../../assets/images/_Avatar share buttonforward.png";
+import dotButton from "../../assets/images/_Avatar share button.png"
 import { handles } from "../../assets/links/links";
 
 const Profile = () => {
   return (
     <div className="profile-container">
+      <div className="dotshare__container">
       <img src={profileImage} alt="Profile image" id="profile__img" />
-
+      <img src={dotButton} alt="avatasharedot" className="avatatdot"/>
+      </div>
+     
       <img className="icon" src={avatar} alt="forward avatar" />
       <div>
         {handles.map((handle, index) => {
